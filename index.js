3753468cff98218710f254bb98cd9eea3dc0c0cc
PierreDemailly <pierredemailly.pro@gmail.com>
2022-09-05 21:12:08 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import { writeFile } from 'node:fs/promises';
+import { Buffer } from 'node:buffer';
+
+const componentName = process.argv[2];
+
+const component = (name) => {
+    return `import { Component } from '@angular/core';
+
+@Component({
+    selector: 'svg-${name}',
+    templateUrl: './${name}.component.svg',
+})
+export class ${name[0].toUpperCase() + name.slice(1)}Component {
+}
+
+`;
+};
+
+const data = new Uint8Array(Buffer.from(component(componentName)));
+
+await  writeFile(`./src/assets/components/${componentName}.component.ts`, data, { flag: 'wx' });
+
+console.log('DONE!');
